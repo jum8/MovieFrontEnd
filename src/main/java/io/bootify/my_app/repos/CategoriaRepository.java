1003a129840e71efb47c197260db02d73aa960ee
jum8 <jmblanco@lasmarias.com.ar>
2023-02-22 09:16:23 -0300

    This is my repository

--- src/main/java/io/bootify/my_app/repos/CategoriaRepository.java
+++ src/main/java/io/bootify/my_app/repos/CategoriaRepository.java
@@ -3,6 +3,6 @@ package io.bootify.my_app.repos;
 import io.bootify.my_app.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+//This is my repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
 }
